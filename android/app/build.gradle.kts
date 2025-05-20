636af61963a6800aa869a50401eecbb453794db8
beamlakefeleke <beamlakefele@gmail.com>
2025-05-20 17:28:25 +0300

    fixing image issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.safaricom_test"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = 26.3.11579264
+    ndkVersion = 26.3;11579264
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
